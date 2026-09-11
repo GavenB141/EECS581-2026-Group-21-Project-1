c4d0ec1c7fb81659c4fb76ed35f3423665f03220
Gaven Behrends <gzbehrends@gmail.com>
2026-09-11 12:30:52 -0500

    raylib scaffold

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+import pyray as rl
+
 def main():
-    print("minesweeper")
+    rl.init_window(640, 480, "Minesweeper")
+    rl.set_target_fps(60)
+
+    while not rl.window_should_close():
+        rl.begin_drawing()
+        rl.clear_background(rl.BLACK)
+        rl.draw_triangle([200, 360], [440, 360], [320, 120], rl.SKYBLUE)
+        rl.end_drawing()
+
+    rl.close_window()
 
 if __name__ == "__main__":
     main()
