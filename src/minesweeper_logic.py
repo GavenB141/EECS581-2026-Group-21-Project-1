b2a86af88c0458e6b1cc3e75d85c04a61eb294bb
Cooperfish <c871f664@ku.edu>
2026-09-17 13:13:48 -0500

    Added place_mines and calc_adjacency

--- src/minesweeper_logic.py
+++ src/minesweeper_logic.py
@@ -1,8 +1,10 @@
 '''
-Author: Max Toney
-Last Modified: 09.16.26
-Modification: Created Logic class with some functions we might need. Created reveal, recursive reveal, and game loss functions.
+Author: Max Toney, Cooper Fish
+Last Modified: 09.17.26
+Modification: Added place_mines as well as calc_adjacency (will do check_win later today)
 '''
+
+import random
 
 class Cell: #creates a cell and its attributes. is it a mine? is it revealed? etc.
     def __init__(self): 
@@ -27,11 +29,35 @@ class Logic: #determines the logic of the game
         self.game_over = False #status of if the game is still ongoing 
         self.grid = [[Cell() for _ in range(self.cols)] for _ in range(self.rows)] #initalize grid
     
-    def place_mines(self):
-        pass #to be written
+    def place_mines(self, safe_row=None, safe_col=None): 
+        all_cells = [(r, c) for r in range(self.rows) for c in range(self.cols)] # builds list of all (r,c)
+        if safe_row is not None and safe_col is not None:
+            all_cells.remove((safe_row, safe_col)) # protect the first click cell from being a mine
+        mine_locations = random.sample(all_cells, self.mine_count) # pick unique random cells for mines
+        for row, col in mine_locations:
+            self.grid[row][col].mine = True # mark that cell as a mine
+        self.calc_adjacency() # calculate adjacency counts now that mines are placed
 
-    def calc_adjacenecy(self):
-        pass #to be written
+    def calc_adjacency(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                cell = self.grid[row][col] # targets the cell we're counting for
+                if cell.mine:
+                    continue # mines don't need a count
+                count = 0
+                for row_change in [-1, 0, 1]: # scan all 8 surrounding cells
+                    for col_change in [-1, 0, 1]:
+                        if row_change == 0 and col_change == 0: 
+                            continue # skip the cell itself
+                        next_row = row + row_change # row of the neighbor being checked
+                        next_col = col + col_change # col of the neighbor being checked
+                        if next_row < 0 or next_row >= self.rows:
+                            continue # stay in bounds vertically
+                        if next_col < 0 or next_col >= self.cols:
+                            continue # stay in bounds horizontally
+                        if self.grid[next_row][next_col].mine:
+                            count += 1 # found a neighboring mine
+                cell.adjacent = count # store the final count on the cell
 
     def reveal_cell(self, row, col):
         cell = self.grid[row][col] #targets the cell we are revealing
