552dacd2ae9c567996cbabe2ec68bb8ed84a38ea
Jocelyn Miller <jdam2k4@ku.edu>
2026-09-18 18:45:37 -0500

    Refactor Grid class for Minesweeper functionality
    Updated the Grid class to implement Minesweeper logic, including mine placement and adjacent mine calculation.

--- src/grid.py
+++ src/grid.py
@@ -1,25 +1,39 @@
 '''
 Author: Jocelyn Miller
-Last Modified: 09.16.26
-Modification: Updated Documentation to work for the Minesweeper Project
+Last Modified: 09.18.26
+Modification: Updated Class logic to work better for Minesweeper
 '''
+import pyray as rl
+from minesweeper_logic import Logic
+
 
 class Grid():
 
-    def __init__(self, file): #Initializes the SodukuSolver class
-        self.file = file
-        self.board = self._read_puzzle()
-        self.solutions = [] # A list to store all solutions
-        
-    def _read_puzzle(self): # 
+    def __init__(self, rows, cols, mines): #Initializes the Grid class
+        self.rows = rows
+        self.cols = cols
+        self.mines = mines
+        self.board = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+        self._place_mines()
+        self._calculate_adjacent_mines()
+
+    def _place_mines(self):
+        # Placeholder for mine placement logic
+        pass
+
+    def _calculate_adjacent_mines(self):
+        # Placeholder for adjacent mine calculation logic
+        pass
         # Reads the puzzle from the file and returns it as a list that the rest of the program can access.
+        # currently commented out as we will not be reading board from a file, but i'm not sure how to change that as of rn.
         board = []
-        with open(self.file, 'r') as f:
-            for line in f:
+        '''
+        with open("puzzle.txt", "r") as file:
                 # This line splits the string by any whitespace, correctly
                 # separating the numbers and underscores.
                 cells = line.strip().split()
                 board.append(cells)
+        '''      
         return board
         
     def print(self): #prints the board to screen (should be edited by other members)
@@ -46,50 +60,6 @@ class Grid():
          #removed the is_in_box function, because it isn't needed for minesweeper
         return True
 
-#The following three functions are not required for minesweeper, but might provide help for starting in other validation classes, please delete comment if unused
-'''    
-    def is_in_row(self, row, num): # checks if a number is already in the row.
-        for col in range(9):
-            if self.board[row][col] == num:
-                return False
-        return True
-    
-    def is_in_col(self, col, num): # checks if a number is already in the column.
-        for row in range(9):
-            if self.board[row][col] == num:
-                return False
-        return True
-    
-    
-    def find_empty_cell(self): # Iterates through the program to find an empty cell and returns it
-        for row in range(9):
-            for col in range(9):
-                if self.is_empty(row, col):
-                    return (row, col)
-        return None
-    
-    def _deep_copy(self): # Sourced from ChatGPT as I am not to use external libraries
-        # This creates a copy of the board(new_board) and then returns it back to self.solve(), which then appends it to the solutions list.
-        new_board = []
-        for row in self.board:
-            new_board.append(row[:])
-        return new_board
-'''
-
-#The following function might be usable to run game state mechanics, but that is up to whoever is coding it.
-    def solve(self):
-        # if there is no empty cells, we append the board to the solutions list
-        find = self.find_empty_cell()
-        if not find:
-            self.solutions.append(self._deep_copy())
-            #uses _deep_copy() to append a copy of the board to the solutions list
-            return
-        else:
-        # starting with the first empty cell we find, we use num to brute force an answer, checking with is_valid() as we go.
-            row, col = find
-            for num in range(1,10):
-                if self.is_valid(row, col, str(num)):
-                    self.board[row][col] = str(num)
-                # Recurses back through .solve() and allows for the program to find multiple solutions.
-                    self.solve()
-                    self.board[row][col] = "_"
+    def render(self): # will render the grid to the screeen
+        # Placeholder for rendering logic
+        pass
