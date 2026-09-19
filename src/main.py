e88ca1c4d20aa3fb70b42c43d5bb5d4e35e4b682
JDAM2K4 <jdam2k4@ku.edu>
2026-09-19 07:28:44 -0500

    Changed logic to just record size of grid and how many mines the grid has.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ Modification: Integrate menu and Logic class
 
 import pyray as rl
 from menu import Menu
+from grid import Grid
 
 def main():
     rl.set_config_flags(rl.ConfigFlags.FLAG_MSAA_4X_HINT 
@@ -15,6 +16,7 @@ def main():
 
     font = rl.load_font("assets/font/NotoSerif-SemiBold.ttf")
     menu = Menu(font)
+    grid = Grid(10, 10, 10)  # Example grid initialization
 
     while not rl.window_should_close():
         # Reset the cursor each frame, so rendering code can set it per-frame
@@ -23,6 +25,7 @@ def main():
         rl.begin_drawing()
         draw_background()
         menu.render()
+        grid.render()
         rl.end_drawing()
 
     rl.unload_font(font)
